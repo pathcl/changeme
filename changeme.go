5519026fc2b2cd9fbf49ffdc24896a70f2d71f7b
Luis San Martin <luis@sanmartin.io>
2019-08-20 17:05:13 +0200

    flags for password length

--- changeme.go
+++ changeme.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/pathcl/go-password/password"
 )
 
 func main() {
-	// Generate a password that is 64 characters long with 10 digits, 10 symbols,
-	// allowing upper and lower case letters, disallowing repeat characters.
-	res, err := password.Generate(32, 10, 10, false, false)
+
+	length := flag.Int("n", 20, "an int between 20 and 64")
+	flag.Parse()
+
+	res, err := password.Generate(*length, 10, 10, false, false)
+
 	if err != nil {
 		log.Fatal(err)
 	}
